dc33df7d1abe9a22b9aab96f53feaa6d0b97465f
cherhy <ekxk1234@naver.com>
2023-06-18 12:54:02 +0900

    권한 관련 로직 수정

--- src/main/java/bucheon/leafy/config/AuthUserDetailService.java
+++ src/main/java/bucheon/leafy/config/AuthUserDetailService.java
@@ -4,16 +4,10 @@ import bucheon.leafy.application.repository.UserRepository;
 import bucheon.leafy.domain.user.User;
 import bucheon.leafy.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
