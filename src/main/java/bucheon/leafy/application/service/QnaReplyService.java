0168b79a416d8f39965b596f4fed893ef8949bfd
junhuijo <jojunhwi89@gamil.com>
2023-06-24 10:05:25 +0900

    수정된 부분반영

---
+++ src/main/java/bucheon/leafy/application/service/QnaReplyService.java
@@ -0,0 +1,53 @@
+package bucheon.leafy.domain.qnareply.service;
+
+import bucheon.leafy.application.mapper.QnaMapper;
+import bucheon.leafy.application.mapper.QnaReplyMapper;
+import bucheon.leafy.domain.qnareply.domain.QnaReplyDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QnaReplyService  {
+
+    @Autowired
+    private final QnaMapper qnaMapper ;
+    @Autowired
+    private final QnaReplyMapper qnaReplyMapper;
+    @Override
+    public int getCount(Integer cid) throws Exception {
+        return qnaReplyMapper.count(cid);
+    }
+    @Override
+    @Transactional()
+    public int remove(Integer cid, Integer bno, String user_id) throws Exception {
+        int rowCnt = qnaMapper.updateCommentCntQna(bno, -1);
+        System.out.println("updateCommentCnt - rowCnt = " + rowCnt);
+        rowCnt = qnaReplyMapper.delete(cid, user_id);
+        System.out.println("rowCnt = " + rowCnt);
+        return rowCnt;
+    }
+    @Override
+    @Transactional
+    public int write(QnaReplyDto qnaReplyDto) throws Exception {
+        qnaMapper.updateCommentCntQna(qnaReplyDto.getId(),1);
+//                throw new Exception("test");
+        return qnaReplyMapper.insert(qnaReplyDto);
+    }
+    @Override
+    public List<QnaReplyDto> getList(Integer cid) throws Exception {
+//        throw new Exception("test");
+        return qnaReplyMapper.selectAll(cid);
+    }
+    @Override
+    public QnaReplyDto read(Integer cid) throws Exception {
+        return qnaReplyMapper.select(cid);
+    }
+    @Override
+    public int modify(QnaReplyDto qnaReplyDto) throws Exception {
+        return qnaReplyMapper.update(qnaReplyDto);
+    }
+}
---
+++ src/main/java/bucheon/leafy/domain/notice/NoticeDto.java
@@ -0,0 +1,19 @@
+package bucheon.leafy.domain.notice.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Data
+public class NoticeDto {
+    private Integer id;
+    private Date created_At;
+    private Date modified_At;
+    private Integer id_delete;
+    private String contents;
+    private Date is_hide;
+    private Date title;
+    private Integer User_user_Id;
+
+}
