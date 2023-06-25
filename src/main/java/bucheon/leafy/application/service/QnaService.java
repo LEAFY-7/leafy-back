2f216c8167ffb3baa64db5457eca6d423f4f33d2
junhuijo <jojunhwi89@gamil.com>
2023-06-26 08:10:02 +0900

    qna 수정

--- src/main/java/bucheon/leafy/application/service/QnaService.java
+++ src/main/java/bucheon/leafy/application/service/QnaService.java
@@ -1,54 +1,57 @@
-//package bucheon.leafy.application.service;
+package bucheon.leafy.application.service;
 
-//import bucheon.leafy.application.mapper.QnaMapper;
-//import bucheon.leafy.domain.qna.QnaDto;
-//import bucheon.leafy.domain.qna.SearchCondition;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class QnaService {
-//
-//    private final QnaMapper qnaMapper;
-//
-//    public int getCount() throws Exception {
-//        return qnaMapper.count();
-//    }
-//    public int remove(Integer id, String userId) throws Exception {
-//        return qnaMapper.delete(id, userId);
-//    }
-//
-//    public int write(QnaDto QnaDto) throws Exception {
-//        return qnaMapper.insert(QnaDto);
-//    }
-//
-//    public int userId(QnaDto qnaDto) throws Exception {
-//        return qnaMapper.insert(qnaDto);
-//    }
-//    public List<QnaDto> getList() throws Exception {
-//        return qnaMapper.selectAll();
-//    }
-//    public QnaDto read(Integer id) throws Exception {
-//        QnaDto qnaDto = qnaMapper.select(id);
-//        qnaMapper.increaseViewCnt(id);
-//        return qnaDto;
-//    }
-//    public List<QnaDto> getPage(Map map) throws Exception {
-//        return qnaMapper.selectPage(map);
-//    }
-//    public int modify(QnaDto qnaDto) throws Exception {
-//        return qnaMapper.update(qnaDto);
-//    }
-//
-//    public List<QnaDto> getsearchSelectPage(SearchCondition sc) throws Exception {
-//        return qnaMapper.searchSelectPage(sc);
-//    }
-//}
+import bucheon.leafy.application.mapper.QnaMapper;
+import bucheon.leafy.domain.qna.QnaDto;
+import bucheon.leafy.domain.qna.SearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class QnaService {
+
+    private final QnaMapper qnaMapper;
+
+    public int getCount() throws Exception {
+        return qnaMapper.count();
+    }
+    public int remove(Integer id, Integer user_user_id) throws Exception {
+        return qnaMapper.delete(id, user_user_id);
+    }
+
+    public int write(QnaDto qnaDto) throws Exception {
+        return qnaMapper.insert(qnaDto);
+    }
+
+    public int userId(QnaDto qnaDto) throws Exception {
+        return qnaMapper.insert(qnaDto);
+    }
+    public List<QnaDto> getList() throws Exception {
+        return qnaMapper.selectAll();
+    }
+    public QnaDto read(Integer id) throws Exception {
+        QnaDto qnaDto = qnaMapper.select(id);
+        return qnaDto;
+    }
+    public List<QnaDto> getPage(Map map) throws Exception {
+        return qnaMapper.selectPage(map);
+    }
+    public int modify(QnaDto qnaDto) throws Exception {
+        return qnaMapper.update(qnaDto);
+    }
+
+    public int getSearchResultCnt(SearchCondition sc) throws Exception {
+        return qnaMapper.searchResultCnt(sc);
+    }
+
+    public List<QnaDto> getSearchSelectPage(SearchCondition sc) throws Exception {
+        return qnaMapper.searchSelectPage(sc);
+    }
+}
