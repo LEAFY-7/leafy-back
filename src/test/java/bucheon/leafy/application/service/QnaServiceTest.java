d75022b69d8c0f650acd19b834db7151d2df8452
junhuijo <jojunhwi89@gamil.com>
2023-06-27 22:00:23 +0900

    커밋

--- src/test/java/bucheon/leafy/application/service/QnaServiceTest.java
+++ src/test/java/bucheon/leafy/application/service/QnaServiceTest.java
@@ -37,50 +37,50 @@ class QnaServiceTest {
     }
 
 
-    @Test
-    public void remove() throws Exception {
-        qnaMapper.deleteAll();
-
-        QnaDto qnaDto = new QnaDto("hello", "hello", "asdf");
-        assertTrue(qnaMapper.insert(qnaDto) == 1);
-        Integer bno = qnaMapper.selectAll().get(0).getId();
-        System.out.println("bno = " + bno);
-
-        commentDao.deleteAll(bno);
-        CommentDto commentDto = new CommentDto(bno,0,"hi","qwer");
-
-        assertTrue(boardDao.select(bno).getComment_cnt() == 0);
-        assertTrue(commentService.write(commentDto)==1);
-        assertTrue(boardDao.select(bno).getComment_cnt() == 1);
-
-        Integer cno = commentDao.selectAll(bno).get(0).getCno();
-
-        // 일부러 예외를 발생시키고 Tx가 취소되는지 확인해야.
-        int rowCnt = commentService.remove(cno, bno, commentDto.getCommenter());
-        assertTrue(rowCnt==1);
-        assertTrue(boardDao.select(bno).getComment_cnt() == 0);
-
-    }
-
-    @Test
-    void write() {
-        boardDao.deleteAll();
-
-        BoardDto boardDto = new BoardDto("hello", "hello", "asdf");
-        assertTrue(boardDao.insert(boardDto) == 1);
-        Integer bno = boardDao.selectAll().get(0).getBno();
-        System.out.println("bno = " + bno);
-
-        commentDao.deleteAll(bno);
-        CommentDto commentDto = new CommentDto(bno,0,"hi","qwer");
-
-        assertTrue(boardDao.select(bno).getComment_cnt() == 0);
-        assertTrue(commentService.write(commentDto)==1);
-
-        Integer cno = commentDao.selectAll(bno).get(0).getCno();
-        assertTrue(boardDao.select(bno).getComment_cnt() == 1);
-    }
-
+//    @Test
+//    public void remove() throws Exception {
+//        qnaMapper.deleteAll();
+//
+//        QnaDto qnaDto = new QnaDto("hello", "hello", "asdf");
+//        assertTrue(qnaMapper.insert(qnaDto) == 1);
+//        Integer bno = qnaMapper.selectAll().get(0).getId();
+//        System.out.println("bno = " + bno);
+//
+//        commentDao.deleteAll(bno);
+//        CommentDto commentDto = new CommentDto(bno,0,"hi","qwer");
+//
+//        assertTrue(boardDao.select(bno).getComment_cnt() == 0);
+//        assertTrue(commentService.write(commentDto)==1);
+//        assertTrue(boardDao.select(bno).getComment_cnt() == 1);
+//
+//        Integer cno = commentDao.selectAll(bno).get(0).getCno();
+//
+//        // 일부러 예외를 발생시키고 Tx가 취소되는지 확인해야.
+//        int rowCnt = commentService.remove(cno, bno, commentDto.getCommenter());
+//        assertTrue(rowCnt==1);
+//        assertTrue(boardDao.select(bno).getComment_cnt() == 0);
+//
+//    }
+//
+//    @Test
+//    void write() {
+//        boardDao.deleteAll();
+//
+//        BoardDto boardDto = new BoardDto("hello", "hello", "asdf");
+//        assertTrue(boardDao.insert(boardDto) == 1);
+//        Integer bno = boardDao.selectAll().get(0).getBno();
+//        System.out.println("bno = " + bno);
+//
+//        commentDao.deleteAll(bno);
+//        CommentDto commentDto = new CommentDto(bno,0,"hi","qwer");
+//
+//        assertTrue(boardDao.select(bno).getComment_cnt() == 0);
+//        assertTrue(commentService.write(commentDto)==1);
+//
+//        Integer cno = commentDao.selectAll(bno).get(0).getCno();
+//        assertTrue(boardDao.select(bno).getComment_cnt() == 1);
+//    }
+//
 
     @Test
     void getList() {
