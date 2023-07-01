32cbcff97e1bf4bb99c4c4db6325231f168f964e
cherhy <ekxk1234@naver.com>
2023-07-02 01:22:21 +0900

    Login 로직 Service 로 이전, userImage의 user_id 컬럼 이슈 수정

--- src/main/java/bucheon/leafy/application/controller/UserController.java
+++ src/main/java/bucheon/leafy/application/controller/UserController.java
@@ -3,20 +3,13 @@ package bucheon.leafy.application.controller;
 import bucheon.leafy.application.service.UserService;
 import bucheon.leafy.domain.user.request.SignInRequest;
 import bucheon.leafy.domain.user.request.SignUpRequest;
-import bucheon.leafy.exception.UserNotFoundException;
-import bucheon.leafy.jwt.JwtFilter;
-import bucheon.leafy.jwt.TokenProvider;
 import bucheon.leafy.jwt.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,32 +26,10 @@ public class UserController {
 
     private final UserService userService;
 
-    private final TokenProvider tokenProvider;
-
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
     @Operation(summary = "로그인")
     @PostMapping("/sign-in")
     public ResponseEntity<TokenResponse> authorize(@Valid @RequestBody SignInRequest signInRequest) {
-
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(signInRequest.getEmail(), signInRequest.getPassword());
-
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String authority = authentication.getAuthorities().stream()
-                .map(g -> g.getAuthority()).findFirst()
-                .orElseThrow(UserNotFoundException::new);
-
-        String role = authority.replace("ROLE_", "");
-
-        String jwt = tokenProvider.createToken(authentication);
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-
-        return new ResponseEntity<>(new TokenResponse(jwt, role), httpHeaders, HttpStatus.OK);
+        return userService.signIn(signInRequest);
     }
 
     @Operation(summary = "아이디 중복체크")
