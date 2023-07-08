a7388fc723108fa587e1f87509d04bdfbbd329da
cherhy <ekxk1234@naver.com>
2023-07-08 23:55:36 +0900

    aws 경로와 설정 추가

--- src/main/java/bucheon/leafy/LeafyApplication.java
+++ src/main/java/bucheon/leafy/LeafyApplication.java
@@ -2,12 +2,17 @@ package bucheon.leafy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 @EnableScheduling
 @EnableAsync
 public class LeafyApplication {
---
+++ src/main/java/bucheon/leafy/path/S3Path.java
@@ -0,0 +1,9 @@
+package bucheon.leafy.path;
+
+public interface S3Path {
+
+    String FEED_PATH = "www.aws.FEED.기타등등";
+    String USER_IMAGE_PATH = "www.aws.USER_IMAGE.기타등등";
+    String USER_BACKGROUND_IMAGE_PATH = "www.aws.USER_BACKGROUND_IMAGE.기타등등";
+
+}
