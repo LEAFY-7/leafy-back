af08aa1ea866f1509ae9bc26d3016b5acebced55
cherhy <ekxk1234@naver.com>
2023-07-10 23:01:46 +0900

    Address 1:N에서 1:1로 수정

--- src/main/java/bucheon/leafy/domain/user/Address.java
+++ src/main/java/bucheon/leafy/domain/user/Address.java
@@ -4,16 +4,22 @@ import bucheon.leafy.domain.user.request.SignUpRequest;
 import bucheon.leafy.util.entity.BaseDeleteEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address extends BaseDeleteEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     private String zipcode;
 
