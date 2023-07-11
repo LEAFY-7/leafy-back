4c60525a2c74bdf90b330d7a93f2600016f38698
hjin <125787016+paramil@users.noreply.github.com>
2023-07-12 01:18:20 +0900

    Update SignUpRequest.java

--- src/main/java/bucheon/leafy/domain/user/request/SignUpRequest.java
+++ src/main/java/bucheon/leafy/domain/user/request/SignUpRequest.java
@@ -1,69 +1,78 @@
 package bucheon.leafy.domain.user.request;
 
 
+import com.nimbusds.openid.connect.sdk.claims.Gender;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 public class SignUpRequest {
 
-    @NotBlank(message = "이메일을 입력해주세요")
-    @Email(message = "이메일 형식을 맞춰주세요")
-    private String email;
+        @NotBlank(message = "이메일을 입력해주세요")
+        @Email(message = "이메일 형식을 맞춰주세요")
+        private String email;
 
-    @NotBlank(message = "비밀번호를 입력해주세요")
-    private String password;
+        @NotBlank(message = "비밀번호를 입력해주세요")
+        private String password;
 
-    @NotBlank(message = "이름을 입력해주세요")
-    private String name;
+        @NotBlank(message = "이름을 입력해주세요")
+        private String name;
 
-    @NotBlank(message = "넥네임을 입력해주세요")
-    private String nickName;
+        @NotBlank(message = "넥네임을 입력해주세요")
+        private String nickName;
+
+        @NotBlank(message = "전화번호를 입력해주세요")
+        private String phone;
+
+        @NotBlank(message = "간단 소개를 입력해주세요")
+        private String simpleIntroduction;
+
+        @NotBlank(message = "생일을 입력해주세요")
+        private LocalDate birthDay;
+
+        @NotBlank(message = "성별을 입력해주세요")
+        private Gender gender;
 
 
-    @NotBlank(message = "넥네임을 입력해주세요")
-    private String nickName;
+        // TODO : 주소
+        private String zipcode;
 
-    @NotBlank(message = "간단 소개를 입력해주세요")
-    private String simpleIntroduction;
+        private String street;
 
-    // TODO : 주소
-    private String zipcode;
+        private String lot;
 
-    @NotBlank(message = "간단 소개를 입력해주세요")
-    private String simpleIntroduction;
+        private String detail;
 
-    // TODO : 주소
-    private String zipcode;
+        private String reference;
 
-    private String street;
+        private Boolean addressIsHide;
 
-    private String lot;
+        @Builder
+        private SignUpRequest(String password, String email, String nickName,  String name,
+                              String simpleIntroduction, String phone, String zipcode,
+                              String street, String lot, String detail, String reference,
+                              Boolean addressIsHide, LocalDate birthDay, Gender gender) {
 
-    private Boolean addressIsHide;
+                this.password = password;
+                this.email = email;
+                this.nickName = nickName;
+                this.name = name;
+                this.simpleIntroduction = simpleIntroduction;
+                this.phone = phone;
+                this.zipcode = zipcode;
+                this.street = street;
+                this.lot = lot;
+                this.detail = detail;
+                this.reference = reference;
+                this.addressIsHide = addressIsHide;
+                this.birthDay = birthDay;
+                this.gender = gender;
+        }
 
-    @Builder
-    private SignUpRequest(String password, String email, String nickName,  String name,
-                          String simpleIntroduction, String phone, String zipcode,
-                          String street, String lot, String detail, String reference,
-                          Boolean addressIsHide) {
-
-            this.password = password;
-            this.email = email;
-            this.nickName = nickName;
-            this.name = name;
-            this.simpleIntroduction = simpleIntroduction;
-            this.phone = phone;
-            this.zipcode = zipcode;
-            this.street = street;
-            this.lot = lot;
-            this.detail = detail;
-            this.reference = reference;
-            this.addressIsHide = addressIsHide;
-    }
 }
