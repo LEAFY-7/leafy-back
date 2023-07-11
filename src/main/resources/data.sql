5ffe9464c86f93f898032ea432506247dd3a5759
cherhy <ekxk1234@naver.com>
2023-07-11 23:51:41 +0900

    Data.sql 파일에 값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,11 @@ VALUES (
        (
            2, now(), now(), false, 'string@gmail.com', '테스트', '$2a$10$48fbkW2.Rt2i9Jyp5a9jPeMNLSMHxOCWo3DSnoiDw.vnsbKaKWkqG', '000000000', 'ADMIN'
        );
+
+INSERT INTO address(created_at, modified_at, is_delete, detail, is_hide, lot, reference, street, zipcode, user_id)
+VALUES (
+            now(), now(), false, 'test1', false, "test1",  "test1",  "test1", "test1",  1
+       ),
+       (
+           now(), now(), false, 'test2', false, "test2",  "test2",  "test2",  "test2",  2
+       );
