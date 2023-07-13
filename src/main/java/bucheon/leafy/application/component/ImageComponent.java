4bda19126c01d94a1c35587715dd0a084ade8eaa
hjin <125787016+paramil@users.noreply.github.com>
2023-07-13 22:02:58 +0900

    ImageComponent 추가

---
+++ src/main/java/bucheon/leafy/application/component/ImageComponent.java
@@ -0,0 +1,71 @@
+package bucheon.leafy.application.component;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ImageComponent {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    public List<String> uploadImage(String imagePath, List<MultipartFile> imageList) {
+
+        List<String> imageNameList = new ArrayList<>();
+
+        for (MultipartFile image : imageList) {
+
+            String imageName = createUUID();
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(image.getSize());
+            metadata.setContentType(image.getContentType());
+
+            try(InputStream inputStream = image.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, imagePath + imageName, inputStream, metadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch(IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
+            }
+
+            imageNameList.add(imageName);
+        }
+
+        return imageNameList;
+    }
+
+    public void deleteImage(String imagePath, String imageName) {
+
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, imagePath + imageName));
+    }
+
+    private String createUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    public String getImageUrl(String imagePath, String imageName) {
+
+        String imageUrl = amazonS3.getUrl(bucket, imagePath + imageName).toString();
+
+        return imageUrl;
+    }
+
+}
