d3c4453fcfefeab47dfc4dd68af2f767e77cebc1
lolmageap <ekxk1234@gmail.com>
2023-07-18 16:48:50 +0900

    PopularTagInformation 과 내부 클래스 (DTO) 추가

--- src/main/java/bucheon/leafy/application/controller/MainController.java
+++ src/main/java/bucheon/leafy/application/controller/MainController.java
@@ -1,7 +1,7 @@
 package bucheon.leafy.application.controller;
 
-import bucheon.leafy.application.service.FeedCommentService;
 import bucheon.leafy.application.service.FeedService;
+import bucheon.leafy.domain.feed.response.PopularTagInformation.PopularTagResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -18,14 +18,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MainController {
 
-    private final FeedCommentService feedCommentService;
     private final FeedService feedService;
 
     @Operation(summary = "메인 페이지 불러오기")
     @GetMapping
     public ResponseEntity getMain() {
-        List<String> popularTags = feedService.getPopularTags();
-
+        List<PopularTagResponse> popularTags = feedService.getPopularTags();
         // TODO : 피드도 넘겨야함
 
         return ResponseEntity.ok().body(popularTags);
