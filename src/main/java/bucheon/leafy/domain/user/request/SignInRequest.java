544c72709d162db5cb125cce17ec9c401b5dc366
cherhy <ekxk1234@naver.com>
2023-07-26 23:56:00 +0900

    signUpRequest 를 매개변수로 하는 of 메서드 생성

--- src/main/java/bucheon/leafy/domain/user/request/SignInRequest.java
+++ src/main/java/bucheon/leafy/domain/user/request/SignInRequest.java
@@ -1,11 +1,14 @@
 package bucheon.leafy.domain.user.request;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 public class SignInRequest {
 
         @Email(message = "이메일 형식을 맞춰주세요")
@@ -14,5 +17,18 @@ public class SignInRequest {
 
         @NotBlank(message = "비밀번호는 필수입니다")
         String password;
+
+        @Builder
+        private SignInRequest(String email, String password) {
+                this.email = email;
+                this.password = password;
+        }
+
+        public static SignInRequest of(SignUpRequest signUpRequest) {
+                return SignInRequest.builder()
+                        .email(signUpRequest.getEmail())
+                        .password(signUpRequest.getPassword())
+                        .build();
+        }
 
 }
