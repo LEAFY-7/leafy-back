ab7ab6e52e717ed73a03069bf726911abab9bcb2
cherhy <ekxk1234@naver.com>
2023-07-26 23:54:30 +0900

    address 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ VALUES (
            2, now(), now(), false, 'string@gmail.com', '테스트', '$2a$10$48fbkW2.Rt2i9Jyp5a9jPeMNLSMHxOCWo3DSnoiDw.vnsbKaKWkqG', '000000000', 'ADMIN' , 'FEMALE', "2023-07-13"
        );
 
-INSERT INTO address(created_at, modified_at, is_delete, detail, is_hide, lot, reference, street, zipcode, user_id)
+INSERT INTO address(created_at, modified_at, is_delete, detail_address, is_hide, road_address, jibun_address, address, zone_code, user_id)
 VALUES (
             now(), now(), false, 'test1', false, "test1",  "test1",  "test1", "test1",  1
        ),
