468efc8d33ad3e834ae6600bd5acdb2d8f558b51
cherhy <ekxk1234@naver.com>
2023-07-29 12:23:01 +0900

    @ApiResponse 를 추가하여 상태코드 설명 출력

--- src/main/java/bucheon/leafy/application/controller/FeedLikeController.java
+++ src/main/java/bucheon/leafy/application/controller/FeedLikeController.java
@@ -4,6 +4,8 @@ import bucheon.leafy.application.service.FeedLikeService;
 import bucheon.leafy.config.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,15 +20,23 @@ public class FeedLikeController {
 
     private final FeedLikeService feedLikeService;
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "좋아요 등록 성공"),
+            @ApiResponse(responseCode = "404", description = "게시글이 삭제됨")
+    })
     @PostMapping
     @Operation(summary = "좋아요 등록")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.CREATED)
     public void like(@AuthenticationPrincipal @Parameter(hidden = true) AuthUser user,
                                        @PathVariable("id") Long feedId) {
         Long userId = user.getUserId();
         feedLikeService.saveLike(userId, feedId);
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "좋아요 삭제 성공"),
+            @ApiResponse(responseCode = "404", description = "게시글이 삭제됨")
+    })
     @DeleteMapping
     @Operation(summary = "좋아요 삭제")
     @ResponseStatus(HttpStatus.NO_CONTENT)
