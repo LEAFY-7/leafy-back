727169de22c40f0eec417a072ba49c1a98eee1d3
cherhy <ekxk1234@naver.com>
2023-07-31 22:54:41 +0900

    static method 작성

--- src/main/java/bucheon/leafy/domain/feed/response/FeedResponse.java
+++ src/main/java/bucheon/leafy/domain/feed/response/FeedResponse.java
@@ -1,11 +1,13 @@
 package bucheon.leafy.domain.feed.response;
 
 
+import bucheon.leafy.domain.feed.Feed;
 import bucheon.leafy.domain.feed.FeedType;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -17,11 +19,12 @@ public class FeedResponse {
     private String title;
     private String content;
     private FeedType feedType;
-    private Date createdAt;
-    private Date modifiedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     @Builder
-    private FeedResponse(Long feedId, Long userId, String title, String content, FeedType feedType, Date createdAt, Date modifiedAt) {
+    private FeedResponse(Long feedId, Long userId, String title, String content,
+                         FeedType feedType, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.feedId = feedId;
         this.userId = userId;
         this.title = title;
@@ -30,4 +33,16 @@ public class FeedResponse {
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
     }
+
+    public static FeedResponse of(Feed feed) {
+        return FeedResponse.builder()
+                .feedId(feed.getId())
+                .title(feed.getTitle())
+                .content(feed.getContent())
+                .feedType(feed.getFeedType())
+                .createdAt(feed.getCreatedAt())
+                .modifiedAt(feed.getModifiedAt())
+                .build();
+    }
+
 }
