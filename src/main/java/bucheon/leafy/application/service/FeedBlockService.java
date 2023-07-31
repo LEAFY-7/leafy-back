53b556814014098cadf4f21bd107ce3765e5562f
cherhy <ekxk1234@naver.com>
2023-07-31 22:53:21 +0900

    피드 차단 로직 작성

--- src/main/java/bucheon/leafy/application/service/FeedBlockService.java
+++ src/main/java/bucheon/leafy/application/service/FeedBlockService.java
@@ -1,10 +1,22 @@
 package bucheon.leafy.application.service;
 
 import bucheon.leafy.application.repository.FeedBlockRepository;
+import bucheon.leafy.application.repository.FeedRepository;
 import bucheon.leafy.application.repository.UserRepository;
+import bucheon.leafy.domain.feed.Feed;
+import bucheon.leafy.domain.feed.response.FeedResponse;
+import bucheon.leafy.domain.feedblock.FeedBlock;
+import bucheon.leafy.domain.user.User;
+import bucheon.leafy.exception.FeedNotFoundException;
+import bucheon.leafy.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -13,6 +25,44 @@ import org.springframework.transaction.annotation.Transactional;
 public class FeedBlockService {
 
     private final UserRepository userRepository;
+    private final FeedRepository feedRepository;
     private final FeedBlockRepository feedBlockRepository;
+
+    public List<FeedResponse> getBlockedFeeds(Long userId, Pageable pageable) {
+        List<FeedBlock> feedBlocks = feedBlockRepository.findByUserId(userId, pageable);
+
+        List<Feed> feeds = feedBlocks.stream()
+                .map(FeedBlock::getFeed)
+                .collect(Collectors.toList());
+
+        List<Long> feedIds = feeds.stream()
+                .map(Feed::getId)
+                .collect(Collectors.toList());
+
+        List<Long> userIds = userRepository.findAllUserIdsByFeedIdIn(feedIds);
+
+        return null;
+    }
+
+    public void blockFeed(Long userId, Long feedId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(FeedNotFoundException::new);
+
+        FeedBlock feedBlock = FeedBlock.of(user, feed);
+        feedBlockRepository.save(feedBlock);
+    }
+
+    public void noneBlockFeed(Long userId, Long feedId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(FeedNotFoundException::new);
+
+        feedBlockRepository.deleteByUserAndFeed(user, feed);
+    }
 
 }
