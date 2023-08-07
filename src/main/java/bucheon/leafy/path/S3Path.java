014cafcb73d0f48cf6cfd0b135280a2d677cde85
lolmageap <ekxk1234@gmail.com>
2023-08-07 15:33:04 +0900

    절대 경로 추가

--- src/main/java/bucheon/leafy/path/S3Path.java
+++ src/main/java/bucheon/leafy/path/S3Path.java
@@ -2,6 +2,7 @@ package bucheon.leafy.path;
 
 public interface S3Path {
 
+    String ABSOLUTE_PATH = "https://bucket-leafy-upload.s3.ap-northeast-2.amazonaws.com/";
     String FEED_PATH = "FEED/";
     String USER_IMAGE_PATH = "USER_IMAGE/";
     String USER_BACKGROUND_IMAGE_PATH = "USER_BACKGROUND_IMAGE/";
