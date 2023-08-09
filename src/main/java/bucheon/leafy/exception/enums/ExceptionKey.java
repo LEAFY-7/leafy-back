9c13ba5cbab94873c0c33c4942e6b684aa339915
cherhy <ekxk1234@naver.com>
2023-08-09 23:48:43 +0900

    따닥 방지 로직 작성

--- src/main/java/bucheon/leafy/exception/enums/ExceptionKey.java
+++ src/main/java/bucheon/leafy/exception/enums/ExceptionKey.java
@@ -7,5 +7,7 @@ public interface ExceptionKey {
     final String NICKNAME = "닉네임";
 
     final String USER_BLOCK = "차단된 회원";
+    final String USER_IMAGE = "프로필 사진";
+    final String USER_BACKGROUND_IMAGE = "배경 사진";
 
 }
