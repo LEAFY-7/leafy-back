1687c09dc8844e0199e0ac2f16be5a1a1ef71864
junhuijo <jojunhwi89@gamil.com>
2023-08-13 14:49:40 +0900

    수정

--- src/main/java/bucheon/leafy/util/request/NoticeSearchRequest.java
+++ src/main/java/bucheon/leafy/util/request/NoticeSearchRequest.java
@@ -1,15 +1,15 @@
-package bucheon.leafy.util.util.request;
+package bucheon.leafy.util.request;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class SearchRequest {
+public class NoticeSearchRequest {
     private String option;
     private String keyword;
 
-    public SearchRequest(String option, String keyword) {
+    public NoticeSearchRequest(String option, String keyword) {
         this.option = option;
         this.keyword = keyword;
     }
