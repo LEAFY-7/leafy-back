144401be369a7c56fb11d8a9a1fce3b9666babb8
cherhy <ekxk1234@naver.com>
2023-08-19 02:42:25 +0900

    예외 처리 로직 작성 및 로깅

--- src/main/java/bucheon/leafy/application/controller/TestController.java
+++ src/main/java/bucheon/leafy/application/controller/TestController.java
@@ -4,13 +4,16 @@ import bucheon.leafy.application.service.TestService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @Tag(name = "테스트")
 @RestController
 @RequestMapping("/api/v1/lets-go")
@@ -19,12 +22,15 @@ public class TestController {
 
     private final TestService testService;
 
-    // admin 으로 만들기
     @Operation(summary = "가즈아!!")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void letsGo(@RequestPart List<MultipartFile> files) {
-        testService.createDummyFeed(files);
+        try {
+            testService.createDummyFeed(files);
+        } catch (IOException e) {
+            log.info("IOException 발생 : {}", e.getMessage());
+        }
     }
 
 }
