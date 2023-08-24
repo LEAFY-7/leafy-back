7b4a001869def0108df425d2276571739bf0feba
cherhy <ekxk1234@naver.com>
2023-08-24 22:47:48 +0900

    수정

--- src/main/java/bucheon/leafy/util/request/PageRequest.java
+++ src/main/java/bucheon/leafy/util/request/PageRequest.java
@@ -2,6 +2,7 @@ package bucheon.leafy.util.request;
 
 import bucheon.leafy.util.SortStatus;
 import bucheon.leafy.util.SortType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 
 @Getter
@@ -9,20 +10,21 @@ public class PageRequest {
 
     private Integer page;
 
-
     public Integer limit;
 
+    @JsonIgnore
     private Long offset;
 
     private String sortColumn;
 
     private SortStatus sortStatus;
 
-    public PageRequest(Integer page, Integer limit, SortType sortType, SortStatus sortStatus) {
+    public PageRequest(Integer page, Integer limit, SortType sortColumn, SortStatus sortStatus) {
+
         this.page = page == null ? 1 : page;
         this.limit = limit == null ? 20 : limit;
         this.offset = Long.valueOf(this.limit * (this.page - 1));
-        this.sortColumn = sortType.getText();
+        this.sortColumn = sortColumn.getText();
         this.sortStatus = sortStatus;
     }
 
