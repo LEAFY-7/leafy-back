13e159a39e02503a705a02c292ffd675f5f056d6
cherhy <ekxk1234@naver.com>
2023-08-25 23:45:20 +0900

    DTO 설명 추가

--- src/main/java/bucheon/leafy/domain/feed/request/FeedRequest.java
+++ src/main/java/bucheon/leafy/domain/feed/request/FeedRequest.java
@@ -2,11 +2,12 @@ package bucheon.leafy.domain.feed.request;
 
 import bucheon.leafy.domain.feed.FeedType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
-import org.hibernate.validator.constraints.Length;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Data
@@ -15,16 +16,34 @@ public class FeedRequest {
 
     @JsonIgnore
     private Long feedId;
+
     private String title;
+
+    @Schema(description = "학명")
     private String species;
+
+    @Schema(description = "별명")
     private String nickname;
+
+    @Schema(description = "온도")
     private Double temperature;
+
+    @Schema(description = "습도")
     private Integer humidity;
+
+    @Schema(description = "물양")
     private Double waterAmount;
+
+    @Schema(description = "물주는기간")
     private String wateringPeriod;
+
+    @Schema(description = "상세입력")
     private String content;
+
     private FeedType feedType;
+
     private List<FeedTagRequest> tagList;
+
     private List<MultipartFile> imageList;
 
     @Builder
