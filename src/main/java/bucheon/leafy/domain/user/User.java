553d8317296fd5b3fe258c2952c2c10964af1d03
cherhy <ekxk1234@naver.com>
2023-08-27 23:18:06 +0900

    Address, Gender 삭제 및 회원가입 시 닉네임 랜덤 생성

--- src/main/java/bucheon/leafy/domain/user/User.java
+++ src/main/java/bucheon/leafy/domain/user/User.java
@@ -10,9 +10,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
 
 @Getter
 @Entity
@@ -38,19 +39,11 @@ public class User extends BaseDeleteEntity {
     @Column(name = "phone", nullable = false)
     private String phone;
 
-    @Enumerated(value = EnumType.STRING)
-    private Gender gender;
-
-    private LocalDate birthDay;
-
     private String introduction;
 
     @JoinColumn(name = "user_id")
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Feed> feeds = new ArrayList<>();
-
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Address address;
 
     // 유저는 정보를 뽑아올 때 거의 대부분 이미지를 뽑아오고 추가적으로 연관관계가 설정이 되어있지 않기 때문에 FetchType.EAGER 로 수정 고려
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
@@ -65,8 +58,7 @@ public class User extends BaseDeleteEntity {
 
     @Builder
     private User(String password, String email, String nickName, String phone,
-                 String name, String introduction, List<Feed> feeds,
-                 Gender gender, LocalDate birthDay, Address address, UserImage userImage,
+                 String name, String introduction, List<Feed> feeds, UserImage userImage,
                  UserBackgroundImage userBackgroundImage, UserRole userRole) {
 
         this.password = password;
@@ -76,42 +68,30 @@ public class User extends BaseDeleteEntity {
         this.phone = phone;
         this.introduction = introduction;
         this.feeds = feeds;
-        this.address = address;
         this.userImage = userImage;
         this.userBackgroundImage = userBackgroundImage;
         this.userRole = userRole;
-        this.gender = gender;
-        this.birthDay = birthDay;
     }
 
     public static User of(SignUpRequest signUpRequest) {
-        Address address = Address.of(signUpRequest);
-
-        User user = User.builder()
+        return User.builder()
                 .password(signUpRequest.getPassword())
                 .email(signUpRequest.getEmail())
                 .name(signUpRequest.getName())
-                .nickName(signUpRequest.getNickName())
+                .nickName(
+                        generateRandomNickname()
+                )
                 .phone(signUpRequest.getPhone())
-                .birthDay(signUpRequest.getBirthDay())
-                .address(address)
                 .userRole(UserRole.MEMBER)
                 .feeds(new ArrayList<>())
                 .build();
-
-        address.addUser(user);
-        return user;
     }
 
     public void update(UserRequest userRequest) {
-        this.address.update(userRequest);
-
         this.name = userRequest.getName();
         this.nickName = userRequest.getNickName();
         this.phone = userRequest.getPhone();
         this.introduction = userRequest.getIntroduction();
-        this.gender = userRequest.getGender();
-        this.birthDay = userRequest.getBirthDay();
     }
 
     public void changePassword(String encodePassword){
@@ -126,8 +106,24 @@ public class User extends BaseDeleteEntity {
         this.userBackgroundImage = null;
     }
 
+    private static String generateRandomNickname() {
+        String randomNickname;
+        do {
+            randomNickname = UUID.randomUUID().toString()
+                    .replace("-", "").substring(0, 12);
+        } while (!isValidNickname(randomNickname));
+        return randomNickname;
+    }
+
+    private static final java.util.regex.Pattern NICKNAME_PATTERN
+            = java.util.regex.Pattern.compile("^(?!admin|leafy)(?!.*\\s{2,})(?!.*\\s$)(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9_가-힣\\s]{3,12}$");
+
+    private static boolean isValidNickname(String nickname) {
+        Matcher matcher = NICKNAME_PATTERN.matcher(nickname);
+        return matcher.matches();
+    }
+
     public void giveRole(){
         this.userRole = UserRole.ADMIN;
     }
-
 }
