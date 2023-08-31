10513d457443e5a8cf4561b4619fef6f17c0539a
junhuijo <junhuijo@gamil.com>
2023-09-01 07:59:17 +0900

    no message

--- src/main/java/bucheon/leafy/domain/comment/request/QnaCommentEditRequest.java
+++ src/main/java/bucheon/leafy/domain/comment/request/QnaCommentEditRequest.java
@@ -3,15 +3,13 @@ package bucheon.leafy.domain.comment.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @NoArgsConstructor
 public class QnaCommentEditRequest {
 
-    private Date comment;
+    private String comment;
 
-    public QnaCommentEditRequest( Date comment) {
+    public QnaCommentEditRequest( String comment) {
         this.comment = comment;
     }
 
