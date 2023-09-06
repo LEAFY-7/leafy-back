e3e846c6f014a0021464d66f23782936bb2ba682
junhuijo <junhuijo@gamil.com>
2023-09-06 23:05:30 +0900

    revert

--- src/main/java/bucheon/leafy/domain/qna/response/QnaResponse.java
+++ src/main/java/bucheon/leafy/domain/qna/response/QnaResponse.java
@@ -19,17 +19,10 @@ public class QnaResponse {
     private String title;
     private Long viewCount;
     private Long userId;
-<<<<<<< HEAD
     private List<QnaCommentResponse> comments;
     private List<QnaReplyResponse> replies;
 
     public QnaResponse(Long qnaId, LocalDateTime createdAt, LocalDateTime modifiedAt, Boolean isDelete, String contents, String title, Long viewCount, Long userId, List<QnaCommentResponse> comments, List<QnaReplyResponse> replies) {
-=======
-    private List<QnaCommentResponse> comments;  // QnaCommentResponse 객체의 리스트
-    private List<QnaReplyResponse> qnaReplies;
-
-    public QnaResponse(Long qnaId, LocalDateTime createdAt, LocalDateTime modifiedAt, Boolean isDelete, String contents, String title, Long viewCount, Long userId, List<QnaCommentResponse> comments, List<QnaReplyResponse> qnaReplies) {
->>>>>>> 485f2d045f4bf943286dcf8c8b58772a12d19092
         this.qnaId = qnaId;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
@@ -39,11 +32,7 @@ public class QnaResponse {
         this.viewCount = viewCount;
         this.userId = userId;
         this.comments = comments;
-<<<<<<< HEAD
         this.replies = replies;
 
-=======
-        this.qnaReplies = qnaReplies;
->>>>>>> 485f2d045f4bf943286dcf8c8b58772a12d19092
     }
 }
