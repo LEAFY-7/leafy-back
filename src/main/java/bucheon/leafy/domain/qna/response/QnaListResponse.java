33fae247d59bb22e0433733e31c3d3fd0b98abcd
junhuijo <junhuijo@gamil.com>
2023-09-07 00:15:17 +0900

    Date로 수정

--- src/main/java/bucheon/leafy/domain/qna/response/QnaListResponse.java
+++ src/main/java/bucheon/leafy/domain/qna/response/QnaListResponse.java
@@ -4,20 +4,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 public class QnaListResponse {
     private Long qnaId;
-    private LocalDateTime createdAt;
-    private LocalDateTime  modifiedAt;
+    private Date createdAt;
+    private Date modifiedAt;
     private Boolean isDelete;
     private String contents;
     private String title;
     private Long viewCount;
     private Long userId;
 
-    public QnaListResponse(Long qnaId, LocalDateTime createdAt, LocalDateTime modifiedAt, Boolean isDelete, String contents, String title, Long viewCount, Long userId) {
+    public QnaListResponse(Long qnaId, Date createdAt, Date modifiedAt, Boolean isDelete, String contents, String title, Long viewCount, Long userId) {
         this.qnaId = qnaId;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
