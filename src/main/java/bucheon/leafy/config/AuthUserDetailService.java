1b24fc0b7b37c10e5a9dd363dc75b14e15894435
lolmageap <ekxk1234@gmail.com>
2023-09-13 09:25:01 +0900

    로그인 관련 로직 수정

--- src/main/java/bucheon/leafy/config/AuthUserDetailService.java
+++ src/main/java/bucheon/leafy/config/AuthUserDetailService.java
@@ -19,13 +19,15 @@ public class AuthUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> oauthUser = userRepository.findByProviderId(username);
-        if (oauthUser.isPresent()){
-            return new AuthUser(oauthUser.get());
-        }
+        User user;
 
-        User user = userRepository.findByEmail(username)
-                .orElseThrow(UserNotFoundException::new);
+        if (username.contains("@")){
+            user = userRepository.findByEmail(username)
+                    .orElseThrow(UserNotFoundException::new);
+        } else {
+            user = userRepository.findByProviderId(username)
+                    .orElseThrow(UserNotFoundException::new);
+        }
 
         return new AuthUser(user);
     }
