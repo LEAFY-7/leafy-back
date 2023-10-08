81837524024b39297efb3180f2c3e0dfbeff2442
cherhy <ekxk1234@naver.com>
2023-10-08 20:30:06 +0900

    이벤트 처리 DTO 생성

---
+++ src/main/java/bucheon/leafy/application/event/LikeCancelEvent.java
@@ -0,0 +1,24 @@
+package bucheon.leafy.application.event;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class LikeCancelEvent {
+
+    private Long feedId;
+    private Long userId;
+
+    @Builder
+    private LikeCancelEvent(Long feedId, Long userId) {
+        this.feedId = feedId;
+        this.userId = userId;
+    }
+
+    public static LikeCancelEvent of(Long feedId, Long userId) {
+        return LikeCancelEvent.builder()
+                .feedId(feedId)
+                .userId(userId)
+                .build();
+    }
+}
