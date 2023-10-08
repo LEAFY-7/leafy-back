cdfb554101cecc646b8fc19f0daf310f86598795
cherhy <ekxk1234@naver.com>
2023-10-09 04:55:17 +0900

    주석 제거

--- src/main/java/bucheon/leafy/application/service/FeedLikeService.java
+++ src/main/java/bucheon/leafy/application/service/FeedLikeService.java
@@ -14,9 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class FeedLikeService {
     private final ApplicationEventPublisher applicationEventPublisher;
 
-
-    // 레코드에 직접적으로 락을 걸기 때문에 성능이 떨어질 수 있음
-    // 추후에 saveLikeInfo 메서드를 다른 트랜잭션(컨트롤러 레이어)으로 변경을 하거나 낙관적 락으로 변경을 고려
     public void saveLike(Long userId, Long feedId) {
         applicationEventPublisher.publishEvent( LikeEvent.of(feedId, userId) );
     }
